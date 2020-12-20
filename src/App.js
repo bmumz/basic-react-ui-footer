080d84862246271140e117888dccede93f8bf10a
bmumz <brandimummery@gmail.com>
2020-12-20 17:14:42 -0500

    Added ability to map through pages as secondary nav.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,66 @@ import './styles/styles.scss';
 
 export default class App extends Component {
   render() {
+    const pages = [
+      {
+        title: 'Company',
+        pageList: [
+          {
+            name: 'About',
+            link: '/about',
+          },
+          {
+            name: 'Locations',
+            link: '/careers',
+          },
+
+          {
+            name: 'Careers',
+            link: '/careers',
+          },
+        ],
+      },
+      {
+        title: 'Legal',
+        pageList: [
+          {
+            name: 'Privacy',
+            link: '/privacy',
+          },
+          {
+            name: 'Terms',
+            link: '/terms',
+          },
+        ],
+      },
+      {
+        title: 'Help Center',
+        pageList: [
+          {
+            name: 'FAQ',
+            link: '/faq',
+          },
+          {
+            name: 'Return Policy',
+            link: '/returnpolicy',
+          },
+        ],
+      },
+    ];
+    console.log(pages);
+
     return (
       <div>
         <BasicFooter
-          title="Logoipsum"
-          subtitle="Made with ❤️ and JavaScript"
+          title="Company name"
+          subtitle="Company slogan/brief about section, capped off at 20% vw to ensure it flows nicely"
           primaryFontColor="#eaeaea"
           secondaryFontColor="#dddddd"
           logo="https://i.ibb.co/r4C7pF2/logoipsum.png"
-          copyright="Brandi Mummery"
+          copyright="Company name"
           copyrightColor="#aaaaaa"
           backgroundColor="#4b4a4a"
+          pages={pages}
           github=""
           linkedin=""
           twitter=""
--- src/BasicFooter.js
+++ src/BasicFooter.js
@@ -18,6 +18,7 @@ export default class BasicFooterComponent extends Component {
       primaryFontColor,
       secondaryFontColor,
       logo,
+      pages,
       copyright,
       copyrightColor,
       backgroundColor,
@@ -30,6 +31,33 @@ export default class BasicFooterComponent extends Component {
       iconSize,
       iconColor,
     } = this.props;
+
+    const info = (
+      <div className="footer__info">
+        {logo !== undefined ? (
+          <img src={logo} alt={title} className="footer__logo" />
+        ) : (
+          ''
+        )}
+        <div>
+          {title !== '' || undefined ? (
+            <h3 style={{ color: primaryFontColor || '#fefefe' }}>{title}</h3>
+          ) : (
+            ''
+          )}
+          {subtitle !== '' || undefined ? (
+            <p
+              className="footer__info--subtitle"
+              style={{ color: secondaryFontColor }}
+            >
+              {subtitle}
+            </p>
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+    );
 
     const socialMedia = (
       <div className="socialIcons" style={{ fontSize: iconSize }}>
@@ -106,6 +134,90 @@ export default class BasicFooterComponent extends Component {
       </div>
     );
 
+    const contact = (
+      <div className="footer__contact">
+        <h4 className="footer__pageTitle">Contact Us!</h4>
+        <h4 className="footer__pageList">555-555-5555</h4>
+        <h4 className="footer__pageList">Email: testing@test.com</h4>
+        <div className="socialIcons" style={{ fontSize: iconSize }}>
+          <a
+            href={facebook}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {facebook !== undefined ? (
+              <FontAwesomeIcon icon={faFacebookSquare} />
+            ) : (
+              ''
+            )}
+          </a>
+          <a
+            href={linkedin}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {linkedin !== undefined ? (
+              <FontAwesomeIcon icon={faLinkedin} />
+            ) : (
+              ''
+            )}
+          </a>
+          <a
+            href={twitter}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {twitter !== undefined ? (
+              <FontAwesomeIcon icon={faTwitterSquare} />
+            ) : (
+              ''
+            )}
+          </a>
+          <a
+            href={angellist}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {angellist !== undefined ? (
+              <FontAwesomeIcon icon={faAngellist} />
+            ) : (
+              ''
+            )}
+          </a>
+          <a
+            href={youtube}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {youtube !== undefined ? (
+              <FontAwesomeIcon icon={faYoutubeSquare} />
+            ) : (
+              ''
+            )}
+          </a>
+          <a
+            href={github}
+            target="_blank"
+            rel="noreferrer"
+            className="socialIcon"
+            style={{ color: iconColor }}
+          >
+            {github !== undefined ? <FontAwesomeIcon icon={faGithub} /> : ''}
+          </a>
+        </div>
+      </div>
+    );
+
     return (
       <div className="test">
         <div
@@ -114,30 +226,42 @@ export default class BasicFooterComponent extends Component {
             backgroundColor: backgroundColor || '#737478',
           }}
         >
-          <div className="footer__firstRowAlt">
-            {logo !== undefined ? (
-              <img src={logo} alt={title} className="footer__logo" />
-            ) : (
-              ''
-            )}
-            {title !== '' || undefined ? (
-              <h2 style={{ color: primaryFontColor || '#e5dcdd' }}>{title}</h2>
-            ) : (
-              ''
-            )}
-            {subtitle !== '' || undefined ? (
-              <p style={{ color: secondaryFontColor }}>{subtitle}</p>
-            ) : (
-              ''
-            )}
-          </div>
+          {pages === undefined ? (
+            <div className="footer__firstRow">{info}</div>
+          ) : (
+            <div className="footer__firstRow">
+              {info}
+
+              {/* {socialMedia} */}
+
+              <div className="footer__pages">
+                {pages.map((page, index) => (
+                  <div key={index} className="footer__pageListTest">
+                    <h4 className="footer__pageTitle">{page.title}</h4>
+                    {page.pageList.map((pageList, index) => (
+                      <div key={index}>
+                        <a
+                          href={`${pageList.link}`}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <h4 className="footer__pageList">{pageList.name}</h4>
+                        </a>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+
+              {contact}
+            </div>
+          )}
 
           <div className="footer__secondRow">
             <p style={{ color: copyrightColor }}>
               {' '}
               © {new Date().getFullYear()} {copyright}
             </p>
-            {socialMedia}
           </div>
         </div>
       </div>
